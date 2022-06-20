c249faf052e9be767bf04ef480b3869078250ef2
KrlozMedina <camedinal@correo.udistrital.edu.co>
2022-06-20 08:58:02 -0500

    [CODE] Cronometro y Timer funcional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-let secondsSpan = document.querySelector('#seconds');
+let hourSpan = document.querySelector('#hours');
 let minutsSpan = document.querySelector('#minuts');
-let timerButton = document.querySelector('#timer-button');
-const reloj = document.querySelector('#reloj');
-let secondsValue = 0;
+let secondsSpan = document.querySelector('#seconds');
+let milisecondsSpan = document.querySelector('#miliseconds');
+
+let hoursValue = 0;
 let minutsValue = 0;
+let secondsValue = 0;
+let milisecondsValue = 0;
+
+let timerButton = document.querySelector('#timer-button');
+const clock = document.querySelector('#clock');
 let currentInterval;
 let currentButton;
 
@@ -15,14 +21,24 @@ function startChronometer(){
     currentButton = event.target;
     currentButton.disabled = true;
     currentInterval = setInterval(() => {
-        secondsValue++;
-        if(secondsValue === 60){
-            secondsValue = 0;
-            minutsValue++;
-            minutsSpan.textContent = formatValue(minutsValue);
+        milisecondsValue++;
+        if(milisecondsValue === 100){
+            milisecondsValue = 0;
+            secondsValue++;
+            if(secondsValue === 60){
+                secondsValue = 0;
+                minutsValue++;
+                if(minutsValue === 60){
+                    minutsValue = 0;
+                    hoursValue++;
+                    hourSpan.textContent = formatValue(hoursValue);
+                }
+                minutsSpan.textContent = formatValue(minutsValue);
+            }
+            secondsSpan.textContent = formatValue(secondsValue);
         }
-        secondsSpan.textContent = formatValue(secondsValue);
-    }, 1000);
+        milisecondsSpan.textContent = formatValue(milisecondsValue);
+    }, 10);
 }
 
 function stopChronometer(){
@@ -31,51 +47,134 @@ function stopChronometer(){
 }
 
 function resetChronometer(){
+    milisecondsValue = 0;
+    milisecondsSpan.textContent = '00';
     secondsValue = 0;
     secondsSpan.textContent = "00";
     minutsValue = 0;
     minutsSpan.textContent = "00";
+    hoursValue = 0;
+    hourSpan.textContent = "00";
+}
+
+function valueIsNull(value){
+    if(value === ''){
+        value = 0;
+    } else {
+    parseInt(value);
+    }
+    return value;
 }
 
 function startTimer(){
     event.preventDefault();
+
+    const hours = valueIsNull(event.target.hours.value);
+    const minutes = valueIsNull(event.target.minutes.value);
+    const seconds = valueIsNull(event.target.seconds.value);
+
+    // console.log(hours, minutes, seconds);
+
+    containerButton = document.querySelector('.container-controls');
+    containerButton.innerHTML = '';
+    button = document.createElement('button');
+    button.type = 'button';
+    button.style.backgroundColor = 'red';
+    button.textContent = 'Stop';
+    button.onclick = () => stopTimer();
+    containerButton.appendChild(button);
+
     // console.log('start');
-    const minutes = parseInt(event.target.minutes.value);
-    const seconds = parseInt(event.target.seconds.value);
+    
     // console.log(minutes, seconds);
-    minutsSpan.textContent = minutes;
-    secondsSpan.textContent = seconds;
+    hourSpan.textContent = formatValue(hours);
+    minutsSpan.textContent = formatValue(minutes);
+    secondsSpan.textContent = formatValue(seconds);
+
+    hoursValue = hours;
     secondsValue = seconds;
     minutsValue = minutes;
 
+    if(hoursValue == null){
+        console.log('entro');
+    }
+
     currentInterval = setInterval(() => {
-        secondsValue--;
         if(secondsValue === 0){
-            secondsValue = 59;
-            minutsValue--;
+            if(minutsValue === 0){
+                if(hoursValue === 0){
+                    stopTimer();
+                    console.log('El timer ha termiando')
+                } else {
+                    hoursValue--;
+                    minutesValue = 59;
+                }
+            } else {
+                minutsValue--;
+                secondsValue = 59;
+            }
+        } else {
+            secondsValue--;
         }
-        if(minutsValue === 0 && secondsValue === 0){
-            alert('Time is over');
-            clearInterval(currentInterval);
-        }
-        minutsSpan.textContent = formatValue(minutsValue);
         secondsSpan.textContent = formatValue(secondsValue);
+        minutsSpan.textContent = formatValue(minutsValue);
+        hourSpan.textContent = formatValue(hoursValue);
     }, 1000);
 }
 
+function stopTimer(){
+    // event.preventDefault();
+    // console.log('stop');
+    clearInterval(currentInterval);
+    minutsSpan.textContent = '00';
+    secondsSpan.textContent = '00';
+    secondsValue = 0;
+    minutsValue = 0;
+
+    containerButton = document.querySelector('.container-controls');
+    containerButton.innerHTML = '';
+    button = document.createElement('button');
+    button.type = 'submit';
+    button.style.backgroundColor = 'green';
+    button.textContent = 'Start';
+    // button.onclick = () => startTimer();
+    containerButton.appendChild(button);
+}
+
+function executeChronometer(){
+    clock.innerHTML =`
+    <h1>Chronometer</h1>
+    <p class="clock-digital">
+        <span id="hours">00</span>:<span id="minuts">00</span>:<span id="seconds">00</span>.<span id="miliseconds">00</span>
+    </p>
+    <div class="container-controls">
+        <button type="button" onclick="startChronometer()" style="background-color: green;">Start</button>
+        <button type="button" onclick="stopChronometer()" style="background-color: red;">Stop</button>
+        <button type="button" onclick="resetChronometer()" style="background-color: yellow;">Reset</button>
+    </div>`
+    hourSpan = document.querySelector('#hours');
+    secondsSpan = document.querySelector('#seconds');
+    minutsSpan = document.querySelector('#minuts');
+    milisecondsSpan = document.querySelector('#miliseconds');
+}
+
 function executeTimer(){
-    reloj.innerHTML =`
+    clock.innerHTML =`
     <h1>Timer</h1>
-    <p>
+    <p class="clock-digital">
         <span id="hours">00</span>:<span id="minuts">00</span>:<span id="seconds">00</span>
     </p>
-    <div>
-    <form onsubmit="startTimer()">
-        <input type="number" placeholder="Minuts" id="minutsInput" name="minutes">
-        <input type="number" placeholder="Seconds" id="secondsInput" name="seconds">
-        <button type="submit">Start</button>
-    </form>
-    </div>`;
+    <form class="formControls" onsubmit="startTimer()">
+        <div class="container-inputs">
+            <input type="number" placeholder="Hrs" id="hoursInput" name="hours">
+            <input type="number" placeholder="Min" id="minutsInput" name="minutes">
+            <input type="number" placeholder="Sec" id="secondsInput" name="seconds">
+        </div>
+        <div class="container-controls">
+            <button type="submit" style="background-color: green;">Start</button>
+        </div>
+    </form>`;
     secondsSpan = document.querySelector('#seconds');
     minutsSpan = document.querySelector('#minuts');
 }
+
