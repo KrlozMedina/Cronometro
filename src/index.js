ebe829fb0906dfccbd50b5fabd479959358532f5
KrlozMedina <charli1216@hotmail.com>
2022-11-07 08:22:08 -0500

    [ADD] Time until holidays and date specials

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-// let hourpan = document.querySelector('#hour');
-// let minutpan = document.querySelector('#minut');
-// let secondpan = document.querySelector('#second');
+const clock = document.querySelector('.clock');
+
+let dateSpan = document.getElementById('date')
+let daySpan = document.getElementById('day');
+let monthSpan = document.getElementById('month');
+let yearSpan = document.getElementById('year')
+let hourSpan = document.getElementById('hour');
+let minutSpan = document.getElementById('minut')
+let secondSpan = document.getElementById('second')
 let milisecondSpan = document.querySelector('#milisecond');
 
+let inputDay = document.getElementById('dayInput');
+let inputMonth = document.getElementById('monthInput');
+let inputYear = document.getElementById('yearInput');
 
 let hourValue = 0;
 let minutValue = 0;
 let secondValue = 0;
 let milisecondValue = 0;
 
-let timerButton = document.querySelector('#timer-button');
-const clock = document.querySelector('.clock');
 let currentInterval;
 let currentButton;
 let menu = false;
 
+const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday']
+const monthString = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+// Functions
+function formatValue(value){
+    return ("0" + value).slice(-2);
+}
+
+function valueIsNull(value){
+    if(value === ''){
+        value = 0;
+    } else {
+    parseInt(value);
+    }
+    return value;
+}
+
+// Menu
 function handleMenu(){
     const menuSpan = document.getElementById('menu');
     if (menu) {
@@ -28,38 +53,21 @@ function handleMenu(){
     menu = !menu;
 }
 
-function formatValue(value){
-    return ("0" + value).slice(-2);
-}
-
-let dateSpan = document.getElementById('date')
-let daySpan = document.getElementById('day');
-let monthSpan = document.getElementById('month');
-let yearSpan = document.getElementById('year')
-let hourSpan = document.getElementById('hour');
-let minutSpan = document.getElementById('minut')
-let secondSpan = document.getElementById('second')
-
-const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday']
-const monthString = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
+// Date and Hour
 function date(){
     currentInterval = setInterval(() => {
         let today = new Date();
         dateSpan.textContent = dayOfWeek[today.getDay()]
-        // daySpan.textContent = moment().format('DD MMM YYYY');
-        daySpan.textContent = formatValue(today.getUTCDate() - 1)
-        // clockSpan.textContent = moment().format('H:mm:ss')
+        daySpan.textContent = formatValue(today.getUTCDate())
         monthSpan.textContent = monthString[today.getMonth()]
         yearSpan.textContent = today.getFullYear()
         hourSpan.textContent = formatValue(today.getHours())
         minutSpan.textContent = formatValue(today.getMinutes())
         secondSpan.textContent = formatValue(today.getSeconds())
-
-        // console.log(today.toString())
     }, 1000);
 }
 
+// Chronometer
 function startChronometer(){
     currentButton = event.target;
     currentButton.disabled = true;
@@ -90,10 +98,14 @@ function stopChronometer(){
 }
 
 function resetChronometer(){
-    // console.log(typeof(currentInterval));
-
     clearInterval(currentInterval);
-    // currentButton.disabled = false;
+
+    try {
+        stopChronometer()
+    } catch (error) {
+
+    }
+    
     milisecondValue = 0;
     milisecondSpan.textContent = "00";
     secondValue = 0;
@@ -104,23 +116,13 @@ function resetChronometer(){
     hourSpan.textContent = "00";
 }
 
-function valueIsNull(value){
-    if(value === ''){
-        value = 0;
-    } else {
-    parseInt(value);
-    }
-    return value;
-}
-
+// Timer
 function startTimer(){
     event.preventDefault();
 
     const hour = valueIsNull(event.target.hour.value);
     const minute = valueIsNull(event.target.minute.value);
     const second = valueIsNull(event.target.second.value);
-
-    // console.log(hour, minutes, seconds);
 
     clockButton = document.querySelector('.clock-controls');
     clockButton.innerHTML = '';
@@ -130,36 +132,32 @@ function startTimer(){
     button.textContent = 'Stop';
     button.onclick = () => stopTimer();
     clockButton.appendChild(button);
-
-    // console.log('start');
-    
-    // console.log(minutes, seconds);
     hourSpan.textContent = formatValue(hour);
     minutSpan.textContent = formatValue(minute);
     secondSpan.textContent = formatValue(second);
-
     hourValue = hour;
     secondValue = second;
     minutValue = minute;
 
-    if(hourValue == null){
-        console.log('entro');
-    }
+    // if(hourValue == null){
+    //     console.log('entro');
+    // }
 
     currentInterval = setInterval(() => {
         if(secondValue === 0){
             if(minutValue === 0){
                 if(hourValue === 0){
                     stopTimer();
-                    console.log('El timer ha termiando')
                 } else {
                     hourValue--;
-                    minutesValue = 59;
+                    minutValue = 59;
+                    secondValue = 59;
                 }
             } else {
                 minutValue--;
                 secondValue = 59;
             }
+            // console.log(minutValue)
         } else {
             secondValue--;
         }
@@ -170,24 +168,107 @@ function startTimer(){
 }
 
 function stopTimer(){
-    // event.preventDefault();
-    // console.log('stop');
     clearInterval(currentInterval);
     minutSpan.textContent = '00';
     secondSpan.textContent = '00';
     secondValue = 0;
     minutValue = 0;
-
     clockButton = document.querySelector('.clock-controls');
     clockButton.innerHTML = '';
     button = document.createElement('button');
     button.type = 'submit';
     button.style.backgroundColor = 'green';
     button.textContent = 'Start';
-    // button.onclick = () => startTimer();
     clockButton.appendChild(button);
 }
 
+// Time for
+function timeFor(day, month, year) {
+    month = month - 1;
+    currentInterval = setInterval(() => {
+        let today = new Date();
+
+        if(year === undefined) {
+            year = today.getFullYear()
+        }
+        if(month === undefined) {
+            month = today.getMonth()
+        }
+        if(day === undefined) {
+            day = today.getDate()
+        }
+
+        let date = new Date(year, month, day);
+        let forDate = date.getTime() - today.getTime()
+
+        let miliseconds = forDate % 1000
+        let secondsForDate = (forDate - miliseconds) / 1000;        
+        let seconds = secondsForDate % 60;
+        let minutsForDate = (secondsForDate - seconds) / 60
+        let minuts = minutsForDate % 60;
+        let hoursForDate = (minutsForDate - minuts) / 60
+        let hours = hoursForDate % 24;
+        let daysForDate = (hoursForDate - hours) / 24
+        let days = daysForDate % 30;
+        let monthsForDate = (daysForDate - days) / 30
+        let months = monthsForDate % 12
+        let yearsForDate = (monthsForDate - months) / 12
+    
+        yearSpan.textContent = yearsForDate;
+        monthSpan.textContent = months;
+        daySpan.textContent = days;
+        hourSpan.textContent = hours;
+        minutSpan.textContent = minuts;
+        secondSpan.textContent = seconds;
+    })
+}
+
+function dateMain(){
+    let dateMain = document.getElementById('selectForDate')
+    // console.log(dateMain.value)
+    clearInterval(currentInterval);
+
+    inputDay = document.getElementById('dayInput');
+    inputMonth = document.getElementById('monthInput');
+    inputYear = document.getElementById('yearInput');
+
+    inputDay.value = ''
+    inputMonth.value = ''
+    inputYear.value = ''
+
+    switch (dateMain.value) {
+        case 'christmas':
+            timeFor(24, 12);
+            break;
+        case 'birthday':
+            timeFor(16, 12);
+            break;
+        case 'endYear':
+            timeFor(31, 12);
+            break;
+        default:
+            break;
+    }
+}
+
+function setDate(){
+    let today = new Date;
+    
+    inputDay = document.getElementById('dayInput');
+    inputMonth = document.getElementById('monthInput');
+    inputYear = document.getElementById('yearInput');
+
+    let setDay = parseInt(inputDay.value) || today.getDate();
+    let setMonth = parseInt(inputMonth.value) || today.getMonth() + 1;
+    let setYear = parseInt(inputYear.value) || today.getFullYear();
+
+    console.log(setDay, setMonth, setYear)
+
+    clearInterval(currentInterval);
+    timeFor(setDay, setMonth, setYear)
+}
+
+// Execute clocks
 function executeChronometer(){
     stopTimer();
 
@@ -232,4 +313,46 @@ function executeTimer(){
     minutSpan = document.querySelector('#minut');
 }
 
+function executeForDate(){
+    stopTimer();
+    resetChronometer();
+
+    clock.innerHTML = `
+    <h1>Time until</h1> 
+    <div class="clock-digital">
+        <div><h6>Years</h6><span id="year">00</span></div>
+        <div><h6>Months</h6><span id="month">00</span></div>
+        <div><h6>Days</h6><span id="day">00</span></div>
+    </div>
+    <div class="clock-digital">
+        <div><h6>Hours</h6><span id="hour">00</span></div>
+        <div><h6>Minuts</h6><span id="minut">00</span></div>
+        <div><h6>Seconds</h6><span id="second">00</span></div>
+    </div>
+    <form action="submit" class="formControls">
+        <div class="clock-inputs">
+            <input onchange="setDate()" id="dayInput" type="text" placeholder="Day">
+            <input onchange="setDate()" id="monthInput" type="text" placeholder="Month">
+            <input onchange="setDate()" id="yearInput" type="text" placeholder="Year">
+        </div>
+    </form>
+    <select onchange="dateMain()" id="selectForDate" name="forDate" id="">
+        <option value="christmas">Christmas</option>
+        <option value="birthday">Krloz's birthday</option>
+        <option value="endYear">End of year</option>
+    </select>
+    <div class="clock-controls"></div>
+    `
+
+    yearSpan = document.getElementById('year');
+    monthSpan = document.getElementById('month');
+    daySpan = document.getElementById('day');
+    hourSpan = document.getElementById('hour');
+    secondSpan = document.getElementById('second');
+    minutSpan = document.getElementById('minut');
+
+    timeFor(24, 12)
+}
+
+// Init
 date()
